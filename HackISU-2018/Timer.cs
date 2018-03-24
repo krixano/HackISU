c5d77e96564f2caa47797e31b1ef2d418d6e151d
Christian Seibold <krixano@outlook.com>
2018-03-24 01:09:27 -0500

    Add Timer class

---
+++ HackISU-2018/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackISU_2018
+{
+    class Timer
+    {
+
+        public int tickAmt;
+        public int currentAmt = 0;
+
+        public Timer(int tickAmt)
+        {
+            this.tickAmt = tickAmt;
+        }
+
+        public bool Update()
+        {
+            this.currentAmt++;
+            if (currentAmt == tickAmt)
+            {
+                this.currentAmt = 0;
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
