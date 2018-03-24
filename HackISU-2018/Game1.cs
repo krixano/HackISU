d99413d123c2a7f2761c6a89d9ec3f0dc9372623
Connor Foster <Connor.Foster@briarcliff.edu>
2018-03-24 06:23:59 -0500

    Somewhat working gun, shooting, rotation, etc

--- HackISU-2018/Game1.cs
+++ HackISU-2018/Game1.cs
@@ -9,7 +9,7 @@ namespace HackISU_2018
     public class Game1 : Game
     {
         public static GamePadState pad1, prevPad1;
-        public static MouseState mouse;
+        public static MouseState mouse, prevMouse;
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         public static KeyboardState keyboard;
@@ -20,6 +20,9 @@ namespace HackISU_2018
         public static Texture2D grassTexture;
         public static Texture2D stoneTexture;
         public static Texture2D gunArmTexture;
+        public static Texture2D bulletTexture;
+
+        
 
         enum GameStates
         {
@@ -36,6 +39,7 @@ namespace HackISU_2018
             public float scale;
             public SpriteEffects effect;
             public float layerDepth;
+            public bool isFired;
         }
 
         public Game1()
@@ -70,6 +74,7 @@ namespace HackISU_2018
             grassTexture = Content.Load<Texture2D>("Grass_Block_Texture_64x64");
             stoneTexture = Content.Load<Texture2D>("Stone_Block_Texture_64x64");
             gunArmTexture = testTexture;
+            bulletTexture = testTexture;
             gun.gunArm.origin.Y = gunArmTexture.Height / 2;
         }
 
@@ -86,7 +91,7 @@ namespace HackISU_2018
             mouse = Mouse.GetState();
             player.playerUpdate();
             gun.gunUpdate();
-
+            prevMouse = mouse;
             prevPad1 = pad1;
             base.Update(gameTime);
         }
@@ -101,6 +106,9 @@ namespace HackISU_2018
                 World.Draw(spriteBatch);
                 spriteBatch.Draw(testTexture, new Rectangle((int) (player.sprite.position.X - (World.offset.X * World.BLOCK_SIZE)), (int) (player.sprite.position.Y - (World.offset.Y * World.BLOCK_SIZE)), (int) player.sprite.size.X, (int) player.sprite.size.Y), Color.White);
                 spriteBatch.Draw(gunArmTexture, new Rectangle((int)(gun.gunArm.position.X - (World.offset.X * World.BLOCK_SIZE)), (int)(gun.gunArm.position.Y - (World.offset.Y * World.BLOCK_SIZE)), (int)gun.gunArm.size.X, (int)gun.gunArm.size.Y), null, Color.Red, gun.gunArm.rotation, gun.gunArm.origin, SpriteEffects.None,0);
+                for (int i = 0; i < gun.bullet.Length; i++)
+                    if (gun.bullet[i].isFired)
+                        spriteBatch.Draw(bulletTexture, new Rectangle((int)(gun.bullet[i].position.X - (World.offset.X * World.BLOCK_SIZE)), (int)(gun.bullet[i].position.Y - (World.offset.Y * World.BLOCK_SIZE)), (int)gun.bullet[i].size.X, (int)gun.bullet[i].size.Y), null, Color.White, gun.bullet[i].rotation, gun.bullet[i].origin, SpriteEffects.None, 0);
             }
             spriteBatch.End();
 
--- HackISU-2018/gun.cs
+++ HackISU-2018/gun.cs
@@ -8,20 +8,60 @@ namespace HackISU_2018
     class gun
     {
         static public Game1.SpriteStruct gunArm;
+        static public Game1.SpriteStruct[] bullet;
+        static public int bulletSpeed;        
+
         static public void gunInit()
         {
+            bulletSpeed = Game1.screenRectangle.Width / 20;
             gunArm.size.X = player.sprite.size.X;
             gunArm.size.Y = gunArm.size.X / 3;
             gunArm.position.X = player.sprite.position.X + (player.sprite.size.X / 2);
             gunArm.position.Y = player.sprite.position.Y + (player.sprite.size.Y / 2);
-            //gunArm.origin.X += gunArm.size.Y;
-            //gunArm.origin.Y += gunArm.size.Y / 2;
+
+            bullet = new Game1.SpriteStruct[100]; 
+            for (int i=0; i< bullet.Length; i++)
+            {
+                bullet[i].isFired = false;
+                bullet[i].size.X = gunArm.size.X / 4;
+                bullet[i].size.Y = gunArm.size.Y / 2;
+                bullet[i].position.X = gunArm.position.X + gunArm.size.X / 2 - bullet[i].size.X / 2;
+                bullet[i].position.Y = gunArm.position.Y + gunArm.size.Y / 2 - bullet[i].size.Y / 2;
+            }
+            
         }
         public static void gunUpdate()
         {
             gunArm.position.X = player.sprite.position.X + gunArm.size.X / 2;
             gunArm.position.Y = player.sprite.position.Y + gunArm.size.Y;
             gunArm.rotation = (float) getMouseAngle();
+
+
+
+            if (Game1.mouse.LeftButton == ButtonState.Pressed)
+            {
+                for (int i = 0; i < bullet.Length; i++)
+                { 
+                bullet[i].position.X = gunArm.position.X + gunArm.size.X / 2 - bullet[i].size.X / 2;
+                bullet[i].position.Y = gunArm.position.Y + gunArm.size.Y / 2 - bullet[i].size.Y / 2;
+                    bullet[i].rotation = gunArm.rotation;
+                }
+                shootGun();
+            }
+
+            for (int i = 0; i < bullet.Length; i++)
+            {
+
+                if (bullet[i].isFired)
+                {
+
+                    bullet[i].position.X += bulletSpeed * (float)Math.Cos(getMouseAngle());
+                    bullet[i].position.Y += bulletSpeed * (float)Math.Sin(getMouseAngle());
+                    if (bullet[i].position.X > Game1.screenRectangle.Width * 2 || bullet[i].position.Y > Game1.screenRectangle.Height * 2
+                        || bullet[i].position.X < Game1.screenRectangle.Width * -2 || bullet[i].position.Y < Game1.screenRectangle.Height * -2)
+                        bullet[i].isFired = false;
+                }
+            }
         }
         public static double getMouseAngle()
         {
@@ -34,6 +74,18 @@ namespace HackISU_2018
             angle = Math.Atan2(distance.Y , distance.X);            
             
             return angle;
+        }
+        public static void shootGun()
+        {
+            for (int i=0; i<bullet.Length; i++)
+            {
+                if (!bullet[i].isFired)
+                {
+                    bullet[i].isFired = true;
+                    bullet[i].rotation = (float)getMouseAngle();
+                    break;
+                }
+            }
         }
     }
 }
