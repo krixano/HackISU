9dcf05d44585c64b5770fadca4db64049f9801f5
Connor Foster <Connor.Foster@briarcliff.edu>
2018-03-24 06:30:30 -0500

    Further merged

--- HackISU-2018/gun.cs
+++ HackISU-2018/gun.cs
@@ -16,9 +16,7 @@ namespace HackISU_2018
             bulletSpeed = Game1.screenRectangle.Width / 20;
             gunArm.size.X = player.sprite.size.X;
             gunArm.size.Y = gunArm.size.X / 3;
-<<<<<<< HEAD
-            gunArm.position.X = player.sprite.position.X + (player.sprite.size.X / 2);
-            gunArm.position.Y = player.sprite.position.Y + (player.sprite.size.Y / 2);
+
 
             bullet = new Game1.SpriteStruct[100]; 
             for (int i=0; i< bullet.Length; i++)
@@ -26,16 +24,16 @@ namespace HackISU_2018
                 bullet[i].isFired = false;
                 bullet[i].size.X = gunArm.size.X / 4;
                 bullet[i].size.Y = gunArm.size.Y / 2;
-                bullet[i].position.X = gunArm.position.X + gunArm.size.X / 2 - bullet[i].size.X / 2;
-                bullet[i].position.Y = gunArm.position.Y + gunArm.size.Y / 2 - bullet[i].size.Y / 2;
+                bullet[i].position_wp.X = gunArm.position_wp.X + gunArm.size.X / 2 - bullet[i].size.X / 2;
+                bullet[i].position_wp.Y = gunArm.position_wp.Y + gunArm.size.Y / 2 - bullet[i].size.Y / 2;
             }
             
-=======
+
             gunArm.position_wp.X = player.sprite.position_wp.X + (player.sprite.size.X / 2);
             gunArm.position_wp.Y = player.sprite.position_wp.Y + (player.sprite.size.Y / 2);
             //gunArm.origin.X += gunArm.size.Y;
             //gunArm.origin.Y += gunArm.size.Y / 2;
->>>>>>> 0a5bfed3705297139090af1cb152b7abe9d630e9
+
         }
         public static void gunUpdate()
         {
@@ -49,8 +47,8 @@ namespace HackISU_2018
             {
                 for (int i = 0; i < bullet.Length; i++)
                 { 
-                bullet[i].position.X = gunArm.position.X + gunArm.size.X / 2 - bullet[i].size.X / 2;
-                bullet[i].position.Y = gunArm.position.Y + gunArm.size.Y / 2 - bullet[i].size.Y / 2;
+                bullet[i].position_wp.X = gunArm.position_wp.X + gunArm.size.X / 2 - bullet[i].size.X / 2;
+                bullet[i].position_wp.Y = gunArm.position_wp.Y + gunArm.size.Y / 2 - bullet[i].size.Y / 2;
                     bullet[i].rotation = gunArm.rotation;
                 }
                 shootGun();
@@ -62,10 +60,10 @@ namespace HackISU_2018
                 if (bullet[i].isFired)
                 {
 
-                    bullet[i].position.X += bulletSpeed * (float)Math.Cos(getMouseAngle());
-                    bullet[i].position.Y += bulletSpeed * (float)Math.Sin(getMouseAngle());
-                    if (bullet[i].position.X > Game1.screenRectangle.Width * 2 || bullet[i].position.Y > Game1.screenRectangle.Height * 2
-                        || bullet[i].position.X < Game1.screenRectangle.Width * -2 || bullet[i].position.Y < Game1.screenRectangle.Height * -2)
+                    bullet[i].position_wp.X += bulletSpeed * (float)Math.Cos(getMouseAngle());
+                    bullet[i].position_wp.Y += bulletSpeed * (float)Math.Sin(getMouseAngle());
+                    if (bullet[i].position_wp.X > Game1.screenRectangle.Width * 2 || bullet[i].position_wp.Y > Game1.screenRectangle.Height * 2
+                        || bullet[i].position_wp.X < Game1.screenRectangle.Width * -2 || bullet[i].position_wp.Y < Game1.screenRectangle.Height * -2)
                         bullet[i].isFired = false;
                 }
             }
